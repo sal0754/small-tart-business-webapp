db2a52c8e45b5fa86392c88ec1c5c5c4e678e400
sal0754 <salmahusban16@gmail.com>
2025-12-05 19:18:28 +1100

    feature: created db and added connection details to config file through env variables and created tart route for viewing tarts in the bakery webapp

---
+++ server/src/routes/tartRoute.js
@@ -0,0 +1,13 @@
+const { Router } = require("express");
+const router = require("express").Router()
+const pool = require("../config/databaseConnection.js")
+
+router.get("/tarts", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM Tart");
+    res.json(result.rows);
+    console.log("tarts selected from tart table in db")
+  } catch (err) {
+    res.status(500).json({ error: "Server error" });
+  }
+});
