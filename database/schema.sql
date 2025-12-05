f3e5555ff1715e93923ae2475b45e527487ab7bb
sal0754 <salmahusban16@gmail.com>
2025-12-05 19:24:36 +1100

    fix: fixed schema, changed keyword names and added order status attribute and fixing name mismatches

--- database/schema.sql
+++ database/schema.sql
@@ -1,35 +1,38 @@
-CREATE TABLE User(
+--Admin users can upload tarts and set their price
+CREATE TABLE IF NOT EXISTS Users(
     user_id SERIAL PRIMARY KEY,
     email VARCHAR(200),
     hashed_password VARCHAR(100),
     user_role VARCHAR(90)
 );
-
-CREATE TABLE Tart(
+-- tart table
+CREATE TABLE IF NOT EXISTS Tart(
     tart_id SERIAL PRIMARY KEY,
-    price FLOAT NOT NULL,
+    price DECIMAL(10,2) NOT NULL,
     tart_type VARCHAR(200)
 
 );
-
-CREATE TABLE CustomerOrder(
+-- table for customer orders 
+CREATE TABLE IF NOT EXISTS CustomerOrder(
     order_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    tart_id INT NOT NULL,
+    order_status VARCHAR(90),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (tart_id) REFERENCES Tart(tart_id)
 
 );
-
-CREATE TABLE OrderItems(
+-- table storing tarts contained in a customer order
+CREATE TABLE IF NOT EXISTS OrderItems(
     order_id INT NOT NULL,
     tart_id INT NOT NULL,
     quantity INT NOT NULL DEFAULT 1,
     PRIMARY KEY (order_id, tart_id),
-    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGN KEY (tart_id) REFERENCES Tarts(tart_id)
+    FOREIGN KEY (order_id) REFERENCES CustomerOrder(order_id),
+    FOREIGN KEY (tart_id) REFERENCES Tart(tart_id)
 );
-
-CREATE TABLE Reviews (
+-- Review table containing customer reviews for tarts
+CREATE TABLE IF NOT EXISTS Reviews (
     review_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     tart_id INT NOT NULL,
@@ -38,6 +41,6 @@ CREATE TABLE Reviews (
     -- picture review contains filepath for uploaded image of tart
     picture_review VARCHAR(300), 
     created_at TIMESTAMP DEFAULT NOW(),
-    FOREIGN KEY (user_id) REFERENCES User(user_id),
+    FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (tart_id) REFERENCES Tart(tart_id)
 );
